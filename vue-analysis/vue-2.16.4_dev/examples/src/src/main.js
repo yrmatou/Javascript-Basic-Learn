83a0e11974da15912dbb0d1222ef5305d337d4de
wangqiao <qiao.wang@custcome-inc.com>
2022-04-02 21:42:02 +0800

    补充vue2源码解析

--- vue-analysis/vue-2.16.4_dev/examples/src/src/main.js
+++ vue-analysis/vue-2.16.4_dev/examples/src/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
 // import router from './router'
 
 // Vue.config.productionTip = false
@@ -17,9 +17,9 @@ new Vue({
   //     }
   //   }, this.message)
   // },
-  mounted () {
-    console.log(this.message)
-  },
+  // mounted () {
+  //   console.log(this.message)
+  // },
   data: {
     message: 'Hello World'
   }
